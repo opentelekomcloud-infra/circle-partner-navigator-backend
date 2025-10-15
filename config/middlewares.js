56f754a32c95975fb71be12b427c875f4dddc1f7
SebastianGode <sebastian@gode-familie.de>
2025-10-15 13:16:36 +0000

    Fix CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
+// ./config/middlewares.js
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://strapi.partners.otc-service.com', 'https://strapi.schreiber-ling.de'],
+      credentials: false,
+      methods: ['GET','POST','PUT','PATCH','DELETE','HEAD','OPTIONS'],
+      headers: ['Content-Type','Authorization','Origin','Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  { name: 'strapi::poweredBy', config: { poweredBy: 'Strapi' } },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  { name: 'strapi::session', config: { secure: true, sameSite: 'lax' } },
   'strapi::favicon',
   'strapi::public',
 ];
